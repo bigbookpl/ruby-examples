77cdfd0de1ea1f99be6c7c92e7aead17fcea8d0b
bigbookpl <bigbook.pl@gmail.com>
2016-03-26 13:21:00 +0100

    Exceptions part 2

--- exceptions.rb
+++ exceptions.rb
@@ -1,19 +1,31 @@
 i = 100
 
+def some_method(i)
+  begin
+
+    i = i+1
+    i / 0 unless i == 110
+    raise Exception.new "Counter #{i}"
+
+    print "i = #{i}"
+    100
+  rescue ZeroDivisionError => e
+    p e.message
+    retry
+  rescue Exception => e
+      p e.message
+      raise Exception.new("Hello it's me")
+  else
+    p "Finished with successfully"
+    101
+  ensure
+    p "This block will be executed always, even if exception"
+    102
+  end
+end
+
 begin
-
-  i = i+1
-  i / 0 unless i == 110
-  raise Exception.new "Counter #{i}"
-
-  print "i = #{i}"
-rescue ZeroDivisionError => e
-  p e.message
-  retry
+  p some_method(i)
 rescue Exception => e
-  p e.message
-else
-  p "Finished with successfully"
-ensure
-  p "This block will be executed always, even if exception"
+  p ">>>>> #{e.message}"
 end
